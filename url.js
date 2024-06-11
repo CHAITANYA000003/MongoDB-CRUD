a68cc749bcbeee5b78291a7ee09cf1d0401e51a7
CHAITANYA000003 <chaitanya2003217@gmail.com>
2024-06-11 14:20:48 +0530

    initial commit

---
+++ url.js
@@ -0,0 +1,4 @@
+// module.exports = `mongodb://localhost:27017`;
+//module.exports = "mongodb+srv://admin:admin@mdb.vtkja.mongodb.net/"
+module.exports =
+  "mongodb+srv://admin:admin@cluster0.42nuott.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
